6939ca06123b9bd6d8e0ab309c67f2a81a7ebe05
mrflick72 <valerio.vaudi@gmail.com>
2020-05-14 19:31:07 +0200

    first commit (project setup)

---
+++ src/main/kotlin/it/valeriovaudi/repositoryservice/RepositoryServiceApplication.kt
@@ -0,0 +1,11 @@
+package it.valeriovaudi.repositoryservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RepositoryServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RepositoryServiceApplication>(*args)
+}
---
+++ src/test/kotlin/it/valeriovaudi/repositoryservice/RepositoryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.valeriovaudi.repositoryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RepositoryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
