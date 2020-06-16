5f595c64c9c417b2548afc7f88bd26caa8e37cc1
mrFlick72 <valerio.vaudi@gmail.com>
2020-06-17 01:00:32 +0200

    download improvement

--- src/main/kotlin/it/valeriovaudi/repositoryservice/DocumentEndPoint.kt
+++ src/main/kotlin/it/valeriovaudi/repositoryservice/DocumentEndPoint.kt
@@ -2,6 +2,7 @@ package it.valeriovaudi.repositoryservice
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.router
@@ -13,15 +14,15 @@ class DocumentEndPoint(private val documentRepository: DocumentRepository) {
     fun messageEndPointRoute() =
             router {
                 GET("/documents/{application}") {
+                    val fileName = it.queryParam("fileName").orElse("")
+                    val fileExtension = it.queryParam("fileExt").orElse("")
                     documentRepository.findOneDocumentFor(
                             Application(it.pathVariable("application")),
                             it.queryParam("path").map { Path(it) }.orElse(Path("")),
-                            FileName(
-                                    it.queryParam("fileName").orElse(""),
-                                    it.queryParam("fileExt").orElse("")
-                            )
+                            FileName(fileName, fileExtension)
                     ).flatMap { fileContent ->
                         ok()
+                                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$fileName.$fileExtension")
                                 .contentType(MediaType.valueOf(fileContent.contentType.value))
                                 .body(BodyInserters.fromValue(fileContent.content))
                     }
