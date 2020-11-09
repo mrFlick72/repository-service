3b0afb3049e3d340fdf8034c87680a81288b712a
mrFlick72 <valerio.vaudi@gmail.com>
2020-11-09 23:36:49 +0100

    not it works!

--- src/main/kotlin/it/valeriovaudi/onlyoneportal/repositoryservice/documents/ESRepository.kt
+++ src/main/kotlin/it/valeriovaudi/onlyoneportal/repositoryservice/documents/ESRepository.kt
@@ -1,28 +1,29 @@
 package it.valeriovaudi.onlyoneportal.repositoryservice.documents
 
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.action.index.IndexResponse
 import org.elasticsearch.action.support.WriteRequest
 import org.springframework.data.elasticsearch.core.ReactiveElasticsearchTemplate
-import reactor.core.publisher.Mono
+import org.springframework.util.IdGenerator
 import reactor.kotlin.core.publisher.toMono
-import java.util.*
 
+class ESRepository(private val reactiveElasticsearchTemplate: ReactiveElasticsearchTemplate,
+                   private val idGenerator: IdGenerator) {
+    fun save(documentMetadata: DocumentMetadata) =
+            reactiveElasticsearchTemplate.execute { client -> client.index(createDocumentOnIndexFor(documentMetadata)) }
+                    .toMono()
+                    .map(this::resultBodyFor)
 
-class ESRepository(private val reactiveElasticsearchTemplate: ReactiveElasticsearchTemplate) {
-    fun save(documentMetadata: DocumentMetadata): Mono<Unit> =
-            reactiveElasticsearchTemplate.execute { client ->
-                client.index { indexRequest ->
-                    indexRequest
-                            .index("application")
-                            .source(documentMetadata.content)
-                            .id(UUID.randomUUID().toString())
-                            .setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE)
-                            .create(true)
+    private fun createDocumentOnIndexFor(documentMetadata: DocumentMetadata): (IndexRequest) -> Unit = { indexRequest ->
+        indexRequest
+                .index("application")
+                .source(documentMetadata.content)
+                .id(idGenerator.generateId().toString())
+                .setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE)
+                .create(true)
+    }
 
-                }
-            }.toMono()
-                    .map { documentMetadata ->
-                        println("documentMetadata.content");
-                        println(documentMetadata);
-                        Unit
-                    }
+    private fun resultBodyFor(documentMetadata: IndexResponse): Map<String, String> =
+            mapOf("index" to documentMetadata.index, "documentId" to documentMetadata.id)
+
 }
--- src/test/kotlin/it/valeriovaudi/onlyoneportal/repositoryservice/documents/ESRepositoryTest.kt
+++ src/test/kotlin/it/valeriovaudi/onlyoneportal/repositoryservice/documents/ESRepositoryTest.kt
@@ -1,45 +1,40 @@
 package it.valeriovaudi.onlyoneportal.repositoryservice.documents
 
-import org.elasticsearch.client.RestClient
-import org.elasticsearch.client.RestHighLevelClient
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.springframework.data.elasticsearch.client.ClientConfiguration
-import org.springframework.data.elasticsearch.client.reactive.ReactiveElasticsearchClient
-import org.springframework.data.elasticsearch.client.reactive.ReactiveRestClients
+import org.springframework.data.elasticsearch.client.ClientConfiguration.builder
+import org.springframework.data.elasticsearch.client.reactive.ReactiveRestClients.create
 import org.springframework.data.elasticsearch.core.ReactiveElasticsearchTemplate
-import org.springframework.http.codec.ClientCodecConfigurer
-import org.springframework.web.reactive.function.client.ExchangeStrategies
-import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.util.IdGenerator
+import reactor.test.StepVerifier
+import java.util.*
+import java.util.function.Consumer
 
 
 internal class ESRepositoryTest {
 
-    fun client(): ReactiveElasticsearchClient? {
-        val clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:39200")
-        /*        .withWebClientConfigurer { webClient: WebClient ->
-                    val exchangeStrategies = ExchangeStrategies.builder()
-                            .codecs { configurer: ClientCodecConfigurer ->
-                                configurer.defaultCodecs()
-                                        .maxInMemorySize(-1)
-                            }
-                            .build()
-                    webClient.mutate().exchangeStrategies(exchangeStrategies).build()
-                }*/
-                .build()
-        return ReactiveRestClients.create(clientConfiguration)
-    }
+    private fun template(): ReactiveElasticsearchTemplate =
+            ReactiveElasticsearchTemplate(
+                    create(
+                            builder()
+                                    .connectedTo("localhost:39200")
+                                    .build()
+                    )
+            )
+
     @Test
     internal fun `save a document on es`() {
-        val defaultReactiveElasticsearchClient = client()!!
-        val reactiveElasticsearchTemplate = ReactiveElasticsearchTemplate(defaultReactiveElasticsearchClient)
-        val esRepository = ESRepository(reactiveElasticsearchTemplate)
+        val id = UUID.randomUUID()
+        val reactiveElasticsearchTemplate = template()
+        val esRepository = ESRepository(reactiveElasticsearchTemplate, IdGenerator { id })
 
-        esRepository.save(DocumentMetadata(mapOf("test" to "test"))).block()
-/*        val stream = StepVerifier.create(
-                esRepository.save(DocumentMetadata(mapOf("test" to "test")))
-        )
-        stream.expectNext(Unit)
-        stream.expectComplete()*/
+        val verifier = StepVerifier.create(esRepository.save(DocumentMetadata(mapOf("test" to "test"))))
+        verifier.assertNext(Consumer {
+            Assertions.assertEquals(mapOf(
+                    "index" to "application",
+                    "documentId" to id.toString()
+            ), it)
+        })
+        verifier.verifyComplete()
     }
 }
