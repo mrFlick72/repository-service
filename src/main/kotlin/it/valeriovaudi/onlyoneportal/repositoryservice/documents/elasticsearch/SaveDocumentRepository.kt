b626001269bffebeed3c62f0b769fb38a7a655df
mrFlick72 <valerio.vaudi@gmail.com>
2021-04-24 15:02:25 +0200

    more log in case of es error during the save document

--- src/main/kotlin/it/valeriovaudi/onlyoneportal/repositoryservice/documents/elasticsearch/SaveDocumentRepository.kt
+++ src/main/kotlin/it/valeriovaudi/onlyoneportal/repositoryservice/documents/elasticsearch/SaveDocumentRepository.kt
@@ -7,19 +7,25 @@ import it.valeriovaudi.onlyoneportal.repositoryservice.documents.Path
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.action.index.IndexResponse
 import org.elasticsearch.action.support.WriteRequest
+import org.slf4j.LoggerFactory
 import org.springframework.data.elasticsearch.core.ReactiveElasticsearchTemplate
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
 class SaveDocumentRepository(private val reactiveElasticsearchTemplate: ReactiveElasticsearchTemplate,
                              private val idGenerator: ESIdGenerator<Triple<Storage, Path, FileName>>) {
+    private val logger = LoggerFactory.getLogger(SaveDocumentRepository::class.java)
+
     fun save(document: Document) =
             saveOnEsFor(document).flatMap { Mono.just(Unit) }
 
     private fun saveOnEsFor(document: Document): Mono<IndexResponse> =
             reactiveElasticsearchTemplate.execute { client ->
                 client.index(indexRequestFor(document))
-            }.toMono().onErrorResume { Mono.empty() }
+            }.toMono().onErrorResume {
+                logger.error(it.message, it)
+                Mono.empty()
+            }
 
 
     private fun indexRequestFor(document: Document): (IndexRequest) -> Unit = { indexRequest ->
